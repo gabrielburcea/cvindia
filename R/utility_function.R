dffc4c1e5a8956f5cc238a7bc6df8a0153ebbce1
Gabriel Burcea <gburcea@ic.ac.uk>
2020-05-06 20:27:05 +0100

    added a new function simpleCap

--- R/utility_function.R
+++ R/utility_function.R
@@ -10,3 +10,19 @@ firstup <- function(x) {
   substr(x, 1, 1) <- toupper(substr(x, 1, 1))
   x
 }
+
+
+
+#' simpleCap 
+#'
+#' @param x 
+#'
+#' @return first letter of a string into upper case 
+#' @export
+#'
+#' @examples
+simpleCap <- function(x) {
+  s <- strsplit(x, " ")[[1]]
+  paste(toupper(substring(s, 1,1)), substring(s, 2),
+        sep="", collapse=" ")
+}

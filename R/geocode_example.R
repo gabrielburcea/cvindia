cb574e446694815c3193fe659cd6129338140259
Gabriel Burcea <gburcea@ic.ac.uk>
2020-05-08 18:39:15 +0100

    uncommented

--- R/geocode_example.R
+++ R/geocode_example.R
@@ -1,12 +1,12 @@
-library(tidyverse)
-library(ggmap)
-
-cities_df <- data %>% 
-  dplyr::distinct(Location)
-
-
-cities_ll_080720 <- mutate_geocode(cities_df, Location)
-
-write_csv(cities_df, path = "D:/Rprojects/cvindia/data/cities_ll_080720.csv")
-
-saveRDS(cities_df, file = "D:/Rprojects/cvindia/data/cities_ll_080720.rds")
+# library(tidyverse)
+# library(ggmap)
+# 
+# cities_df <- data %>% 
+#   dplyr::distinct(Location)
+# 
+# 
+# cities_ll_080720 <- mutate_geocode(cities_df, Location)
+# 
+# write_csv(cities_df, path = "D:/Rprojects/cvindia/data/cities_ll_080720.csv")
+# 
+# saveRDS(cities_df, file = "D:/Rprojects/cvindia/data/cities_ll_080720.rds")

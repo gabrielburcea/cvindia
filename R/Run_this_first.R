64e3e90b1b2917e2f78858c8e2c769ad213ce6d2
Gabriel Burcea <gabrielburcea@gabriels-mbp.home>
2020-05-18 15:19:58 +0100

    added new codes for the purpose of creating data for modeling

--- R/Run_this_first.R
+++ R/Run_this_first.R
@@ -1,31 +1,74 @@
-# data_select <- PivotMappe060520r %>%
-#   dplyr::select(ID, Age, Gender, Location, Country, Chills, Cough, Diarrhoea, Fatigue, Headcahe, 'Healthcare Worker', 'How Unwell',
-#               'Long Standing Health Issues', 'Loss of smell and taste', 'Muscle Ache', 'Nasal Congestion', 'Nausea and Vomiting', 'Number Of Days Symptoms Showing',
-#               'Pregnant', 'Self Diagnosis', 'Shortness of Breath', 'Sore Throat','Reason For Helping', 'Sputum', 'Temperature') %>%
-#   dplyr::rename( id = ID,
-#                  age = Age,
-#                  gender = Gender,
-#                  location =  Location,
-#                  country = Country,
-#                  chills = Chills,
-#                  diarrhoea = Diarrhoea,
-#                  fatigue = Fatigue,
-#                  headache = Headcahe,
-#                  healthcare_worker = 'Healthcare Worker',
-#                  how_unwell = 'How Unwell',
-#                  long_standing_health = 'Long Standing Health Issues',
-#                  loss_smell_taste = 'Loss of smell and taste',
-#                  muscle_ache = 'Muscle Ache',
-#                  nasal_congestion = 'Nasal Congestion',
-#                  nausea_vomiting = 'Nausea and Vomiting',
-#                  no_days_symptoms_show = 'Number Of Days Symptoms Showing',
-#                  pregnant =  'Pregnant',
-#                  shortness_breath = 'Shortness of Breath',
-#                  sore_throat = 'Sore Throat',
-#                  sputum = 'Sputum',
-#                  temperature = 'Temperature',
-#                  self_diagnosis = 'Self Diagnosis',
-#                  tested_or_not = 'Reason For Helping')
+library(tidyverse)
+
+data <- PivotMappe060520r
+
+data_select <- data %>%
+  dplyr::select(ID, Age, Gender, Location, Country, Chills, Cough, Diarrhoea, Fatigue, Headcahe, 'Healthcare Worker', 'How Unwell',
+              'Long Standing Health Issues', 'Loss of smell and taste', 'Muscle Ache', 'Nasal Congestion', 'Nausea and Vomiting', 'Number Of Days Symptoms Showing',
+              'Pregnant', 'Self Diagnosis', 'Shortness of Breath', 'Sore Throat','Reason For Helping', 'Sputum', 'Temperature') %>%
+  dplyr::rename( id = ID,
+                 age = Age,
+                 gender = Gender,
+                 location =  Location,
+                 country = Country,
+                 chills = Chills,
+                 diarrhoea = Diarrhoea,
+                 fatigue = Fatigue,
+                 headache = Headcahe,
+                 healthcare_worker = 'Healthcare Worker',
+                 how_unwell = 'How Unwell',
+                 long_standing_health = 'Long Standing Health Issues',
+                 loss_smell_taste = 'Loss of smell and taste',
+                 muscle_ache = 'Muscle Ache',
+                 nasal_congestion = 'Nasal Congestion',
+                 nausea_vomiting = 'Nausea and Vomiting',
+                 no_days_symptoms_show = 'Number Of Days Symptoms Showing',
+                 pregnant =  'Pregnant',
+                 shortness_breath = 'Shortness of Breath',
+                 sore_throat = 'Sore Throat',
+                 sputum = 'Sputum',
+                 temperature = 'Temperature',
+                 self_diagnosis = 'Self Diagnosis',
+                 tested_or_not = 'Reason For Helping')
+
+data_select$tested_or_not <- as.factor(data_select)
+
+test_data <- data_select %>%
+  tidyr::separate('long_standing_health', c('comorbidity_one', 'comorbidity_two', 'comorbidity_three', 'comorbidity_four'), sep = ",") %>%
+  tidyr::replace_na(list('comorbidity_one' = 'None', 'comorbidity_two' = 'None', 'comorbidity_three' = 'None', 'comorbidity_four' = 'None'))
+
+test_data[test_data == 0] <- NA
+
+data_model <- test_data %>%
+  dplyr::mutate(tested_covid = stringr::str_match(tested_or_not, 'Positive')) %>%
+  dplyr::mutate(tested_covid = stringr::str_match(tested_or_not, 'Positive')) %>%
+  tidyr::replace_na(list('tested_covid' = 'Negative')) %>%
+  tidyr::replace_na(list('no_days_symptoms_show' = 'None' )) %>%
+  dplyr::select(id, age, gender, country, chills, Cough, diarrhoea, fatigue, healthcare_worker, 
+                how_unwell, comorbidity_one, loss_smell_taste, muscle_ache, nasal_congestion, nausea_vomiting, 
+                no_days_symptoms_show, self_diagnosis, shortness_breath, sore_throat, sputum, temperature, tested_covid)
+  
+
+# write.csv(data_model, file = "/Users/gabrielburcea/Rprojects/data/data_model.csv")
+# count_positive_negative <- test_data %>% 
+#   dplyr::select(id, tested_covid) %>%
+#   dplyr::group_by(tested_covid) %>%
+#   tally()
+
+# asthma <- test_data %>%
+#   dplyr::filter(comorbidity_one == 'Asthma (managed with an inhaler)' | comorbidity_one == "None") 
+# 
+# 
+# long_standing_lung_condition <- test_data %>%
+#   dplyr::filter(comorbidity_one == 'Long-Standing Lung Condition' | comorbidity_one == 'None')
+# 
+# high_blood_pressure <- test_data %>% 
+#   dplyr::filter(comorbidity_one == 'High Blood Pressure (hypertension)' | comorbidity_one == "None") 
+# 
+# obesity <- test_data %>%
+#   dplyr::filter(comorbidity_one == 'Obesity' | comorbidity_one == "None") 
+
+
 
 
 # dt_select <- dt_select %>%

5d6cd94afe55fd3ae4e81a4ec10326b66202fdac
Gabriel Burcea <gabrielburcea@gabriels-mbp.home>
2020-06-18 21:02:56 +0100

    confusion of what is to be used sneaked in, thus rethought the script to keep the levels of severity for the plots only.

--- R/Run_this_first.R
+++ R/Run_this_first.R
@@ -95,84 +95,84 @@
 #                                      age >= 95  ~ '95+'))
 
 
-# data_rec <- read.csv("/Users/gabrielburcea/Rprojects/data/PivotMappe060520r.csv", header = TRUE, sep = ",")
-# data_select <- data_rec %>%
-#   #### rename reason for helping as it contains whether respondents have been tested negative or positive
-#   dplyr::rename(tested_or_not = 'Reason.For.Helping') %>%
-#   tidyr::separate('Long.Standing.Health.Issues', c('Comorbidity_one', 'Comorbidity_two', 'Comorbidity_three', 'Comorbidity_four',
-#                                                    'Comorbidity_five', 'Comorbidity_six', 'Comorbidity_seven', 'Comorbidity_eight',
-#                                                    'Comorbidity_nine'), sep = ",")
-# data_rec <- data_select  %>%
-#   tidyr::pivot_longer(cols = starts_with('Comorbidity'),
-#                       names_to = 'Comorbidity_count',
-#                       values_to = 'Comorbidity') %>%
-#   tidyr::drop_na('Comorbidity') %>%
-#   dplyr::select(-Comorbidity_count) %>%
-#   dplyr::distinct() %>%
-#   dplyr::mutate(Condition = 'Yes') %>%
-#   tidyr::pivot_wider(id_cols = -c(Comorbidity, Condition), names_from = Comorbidity, values_from = Condition, values_fill = list(Condition = 'No')) %>%
-#   dplyr::select(-None)
+data_rec <- read.csv("/Users/gabrielburcea/Rprojects/data/PivotMappe060520r.csv", header = TRUE, sep = ",")
+data_select <- data_rec %>%
+  #### rename reason for helping as it contains whether respondents have been tested negative or positive
+  dplyr::rename(tested_or_not = 'Reason.For.Helping') %>%
+  tidyr::separate('Long.Standing.Health.Issues', c('Comorbidity_one', 'Comorbidity_two', 'Comorbidity_three', 'Comorbidity_four',
+                                                   'Comorbidity_five', 'Comorbidity_six', 'Comorbidity_seven', 'Comorbidity_eight',
+                                                   'Comorbidity_nine'), sep = ",")
+data_rec <- data_select  %>%
+  tidyr::pivot_longer(cols = starts_with('Comorbidity'),
+                      names_to = 'Comorbidity_count',
+                      values_to = 'Comorbidity') %>%
+  tidyr::drop_na('Comorbidity') %>%
+  dplyr::select(-Comorbidity_count) %>%
+  dplyr::distinct() %>%
+  dplyr::mutate(Condition = 'Yes') %>%
+  tidyr::pivot_wider(id_cols = -c(Comorbidity, Condition), names_from = Comorbidity, values_from = Condition, values_fill = list(Condition = 'No')) %>%
+  dplyr::select(-None)
 
 
 
 ########################################
 #### Loading libraries needed ##########
 ########################################
-# library(caret)
-# library(corrplot)
-# library(tidyverse)
-# library(e1071)
-# library(DMwR)
-# library(lattice)
-# library(pROC)
-# library(ROCR)
-# library(ggplot2)
-# library(reshape2)
-# library(leaps)
-# library(MASS)
-# library(rms)
-# level_key <-
-#   c(
-#     "None" = 'none',
-#     "Curious" = "none",
-#     'Showing Symptoms But Not Tested,Curious' = "showing symptoms",
-#     'Showing Symptoms But Not Tested' = "showing symptoms",
-#     'Self-Isolating With No Symptoms' = "none",
-#     'Showing Symptoms But Not Tested,Curious,Self-Isolating With No Symptoms' = "showing symptoms",
-#     'Tested Positive' = 'positive',
-#     'Curious,Self-Isolating With No Symptoms' = 'none',
-#     'Tested Negative But Have Symptoms' = 'showing symptoms',
-#     'Recovered But Have New Symptoms' = 'positive',
-#     'Live With Someone With Coronavirus' = 'live_with_scorona',
-#     'Live With Someone With Coronavirus,Curious' = 'live_with_scorona',
-#     'Tested Negative But Have Symptoms,Self-Isolating With No Symptoms' = 'showing symptoms',
-#     'Tested Negative But Have Symptoms,Curious' = 'showing symptoms',
-#     'Tested Positive,Tested Negative But Have Symptoms,Showing Symptoms But Not Tested' = 'positive',
-#     'Tested Positive,Self-Isolating With No Symptoms' = 'positive',
-#     'Showing Symptoms But Not Tested,Self-Isolating With No Symptoms' = 'showing symptoms',
-#     'Tested Positive,Tested Negative But Have Symptoms,Showing Symptoms But Not Tested,Recovered But Have New Symptoms,Curious,Self-Isolating With No Symptoms' = 'positive',
-#     'Tested Negative But Have Symptoms,Showing Symptoms But Not Tested' = 'showing symptoms',
-#     'Showing Symptoms But Not Tested,Recovered But Have New Symptoms' = 'showing symptoms',
-#     'Tested Positive,Curious' = 'positive',
-#     'Tested Positive,Showing Symptoms But Not Tested' = 'positive',
-#     'Tested Positive,Live With Someone With Coronavirus' = 'positive',
-#     'Recovered But Have New Symptoms,Curious' = 'positive',
-#     'Live With Someone With Coronavirus,Self-Isolating With No Symptoms' = 'live_with_scorona',
-#     'Tested Positive,Recovered But Have New Symptoms' = 'positive',
-#     'Live With Someone With Coronavirus,Curious,Self-Isolating With No Symptoms' = 'live_with_scorona',
-#     'Showing Symptoms But Not Tested,Recovered But Have New Symptoms,Curious' = 'showing symptoms',
-#     'Recovered But Have New Symptoms,Self-Isolating With No Symptoms' = 'showing symptoms',
-#     'Recovered But Have New Symptoms,Curious,Self-Isolating With No Symptoms' = 'showing symptoms',
-#     'Tested Positive,Tested Negative But Have Symptoms,Recovered But Have New Symptoms' = 'showing symptoms',
-#     'Tested Positive,Tested Negative But Have Symptoms,Showing Symptoms But Not Tested,Live With Someone With Coronavirus,Recovered But Have New Symptoms,Curious,Self-Isolating With No Symptoms' = 'positive',
-#     'Tested Positive,Tested Negative But Have Symptoms,Showing Symptoms But Not Tested,Curious' = 'positive',
-#     'Tested Positive,Tested Negative But Have Symptoms' = 'positive',
-#     'Tested Negative But Have Symptoms,Showing Symptoms But Not Tested,Recovered But Have New Symptoms,Curious,Self-Isolating With No Symptoms' = 'showing symptoms',
-#     'Showing Symptoms But Not Tested,Live With Someone With Coronavirus' = 'showing symptoms',
-#     'Tested Positive,Recovered But Have New Symptoms,Curious' = 'positive',
-#     'Tested Negative But Have Symptoms,Curious,Self-Isolating With No Symptoms' = 'showing symptoms',
-#     'Tested Negative But Have Symptoms,Showing Symptoms But Not Tested,Curious'  = 'showing symptoms'
-#   )
+library(caret)
+library(corrplot)
+library(tidyverse)
+library(e1071)
+library(DMwR)
+library(lattice)
+library(pROC)
+library(ROCR)
+library(ggplot2)
+library(reshape2)
+library(leaps)
+library(MASS)
+library(rms)
+level_key <-
+  c(
+    "None" = 'none',
+    "Curious" = "none",
+    'Showing Symptoms But Not Tested,Curious' = "showing symptoms",
+    'Showing Symptoms But Not Tested' = "showing symptoms",
+    'Self-Isolating With No Symptoms' = "none",
+    'Showing Symptoms But Not Tested,Curious,Self-Isolating With No Symptoms' = "showing symptoms",
+    'Tested Positive' = 'positive',
+    'Curious,Self-Isolating With No Symptoms' = 'none',
+    'Tested Negative But Have Symptoms' = 'showing symptoms',
+    'Recovered But Have New Symptoms' = 'positive',
+    'Live With Someone With Coronavirus' = 'live_with_scorona',
+    'Live With Someone With Coronavirus,Curious' = 'live_with_scorona',
+    'Tested Negative But Have Symptoms,Self-Isolating With No Symptoms' = 'showing symptoms',
+    'Tested Negative But Have Symptoms,Curious' = 'showing symptoms',
+    'Tested Positive,Tested Negative But Have Symptoms,Showing Symptoms But Not Tested' = 'positive',
+    'Tested Positive,Self-Isolating With No Symptoms' = 'positive',
+    'Showing Symptoms But Not Tested,Self-Isolating With No Symptoms' = 'showing symptoms',
+    'Tested Positive,Tested Negative But Have Symptoms,Showing Symptoms But Not Tested,Recovered But Have New Symptoms,Curious,Self-Isolating With No Symptoms' = 'positive',
+    'Tested Negative But Have Symptoms,Showing Symptoms But Not Tested' = 'showing symptoms',
+    'Showing Symptoms But Not Tested,Recovered But Have New Symptoms' = 'showing symptoms',
+    'Tested Positive,Curious' = 'positive',
+    'Tested Positive,Showing Symptoms But Not Tested' = 'positive',
+    'Tested Positive,Live With Someone With Coronavirus' = 'positive',
+    'Recovered But Have New Symptoms,Curious' = 'positive',
+    'Live With Someone With Coronavirus,Self-Isolating With No Symptoms' = 'live_with_scorona',
+    'Tested Positive,Recovered But Have New Symptoms' = 'positive',
+    'Live With Someone With Coronavirus,Curious,Self-Isolating With No Symptoms' = 'live_with_scorona',
+    'Showing Symptoms But Not Tested,Recovered But Have New Symptoms,Curious' = 'showing symptoms',
+    'Recovered But Have New Symptoms,Self-Isolating With No Symptoms' = 'showing symptoms',
+    'Recovered But Have New Symptoms,Curious,Self-Isolating With No Symptoms' = 'showing symptoms',
+    'Tested Positive,Tested Negative But Have Symptoms,Recovered But Have New Symptoms' = 'showing symptoms',
+    'Tested Positive,Tested Negative But Have Symptoms,Showing Symptoms But Not Tested,Live With Someone With Coronavirus,Recovered But Have New Symptoms,Curious,Self-Isolating With No Symptoms' = 'positive',
+    'Tested Positive,Tested Negative But Have Symptoms,Showing Symptoms But Not Tested,Curious' = 'positive',
+    'Tested Positive,Tested Negative But Have Symptoms' = 'positive',
+    'Tested Negative But Have Symptoms,Showing Symptoms But Not Tested,Recovered But Have New Symptoms,Curious,Self-Isolating With No Symptoms' = 'showing symptoms',
+    'Showing Symptoms But Not Tested,Live With Someone With Coronavirus' = 'showing symptoms',
+    'Tested Positive,Recovered But Have New Symptoms,Curious' = 'positive',
+    'Tested Negative But Have Symptoms,Curious,Self-Isolating With No Symptoms' = 'showing symptoms',
+    'Tested Negative But Have Symptoms,Showing Symptoms But Not Tested,Curious'  = 'showing symptoms'
+   )
 
 # #################################################
 # ######### Get a numeric dataset #################
@@ -251,68 +251,52 @@
 #      'Yes' = "Mild",
 #      'Yes' = "Moderate",
 #      'Yes' = "Severe")
-# level_key_temperature <-
-#   c( "38.1-39" = '38.2-39',
-#      "38.1-39" = 'Temperature')
+level_key_temperature <-
+  c( "38.1-39" = '38.2-39',
+     "38.1-39" = 'Temperature')
+
 # level_key_care_home_worker <-
 #   c('Yes' = 'Yes',
 #     'No' = 'No')
-# level_no_days_sympt_show <-
-#   c('21' = "More than 21")
-# data_model <- data_rec %>%
-#   dplyr::mutate(Covid_tested = dplyr::recode(tested_or_not, !!!level_key))
+level_no_days_sympt_show <-
+  c('21' = "More than 21")
 
-# data_model$gender <- as.factor(data_model$Gender)
-# data_model$country <- as.factor(data_model$Country)
-# data_model$chills <- as.factor(data_model$Chills)
-# data_model$cough  <- as.factor(data_model$Cough)
-# data_model$diarrhoea  <- as.factor(data_model$Diarrhoea)
-# data_model$fatigue  <- as.factor(data_model$Fatigue)
-# data_model$headache   <- as.factor(data_model$Headcahe)
-# data_model$loss_smell_taste   <- as.factor(data_model$Loss.of.smell.and.taste)
-# data_model$muscle_ache  <- as.factor(data_model$Muscle.Ache)
-# data_model$nasal_congestion <- as.factor(data_model$Nasal.Congestion)
-# data_model$nausea_vomiting  <- as.factor(data_model$Nausea.and.Vomiting)
-# data_model$self_diagnosis <- as.factor(data_model$Self.Diagnosis)
-# data_model$shortness_breath <- as.factor(data_model$Shortness.of.Breath)
-# data_model$sore_throat <- as.factor(data_model$Sore.Throat)
-# data_model$sputum <- as.factor(data_model$Sputum)
-# data_model$temperature  <- as.factor(data_model$Temperature)
-# data_model$health_care_worker <- as.factor(data_model$Healthcare.Worker)
-# data_model$care_home_worker <- as.factor(data_model$Care.Home.Worker)
+data_model <- data_rec %>%
+  dplyr::mutate(Covid_tested = dplyr::recode(tested_or_not, !!!level_key), 
+                number_days_symptoms = forcats::fct_recode(Number.Of.Days.Symptoms.Showing, !!!level_no_days_sympt_show) )
 
-# data_model$asthma   <- as.factor(data_model$`Asthma (managed with an inhaler)`)
-# data_model$diabetes_type_two <- as.factor(data_model$`Diabetes Type 2`)
-# data_model$obesity <- as.factor(data_model$Obesity)
-# data_model$hypertension  <- as.factor(data_model$`High Blood Pressure (hypertension)`)
-# data_model$heart_disease  <- as.factor(data_model$`Long-Standing Heart Disease`)
-# data_model$kidney_disease <- as.factor(data_model$`Long-Standing Kidney Disease`)
-# data_model$lung_condition <- as.factor(data_model$`Long-Standing Lung Condition`)
-# data_model$liver_disease <- as.factor(data_model$`Long-Standing Liver Disease`)
-# data_model$diabetes_type_one <- as.factor(data_model$`Diabetes Type 1 (controlled by insulin)`)
-# data_model$how_unwell <- as.factor(data_model$How.Unwell)
-# data_model$age <- as.factor(data_model$Age)
-# data_model$covid_tested <- as.factor(data_model$Covid_tested)
+data_model$gender <- as.factor(data_model$Gender)
+data_model$country <- as.factor(data_model$Country)
+data_model$chills <- as.factor(data_model$Chills)
+data_model$cough  <- as.factor(data_model$Cough)
+data_model$diarrhoea  <- as.factor(data_model$Diarrhoea)
+data_model$fatigue  <- as.factor(data_model$Fatigue)
+data_model$headache   <- as.factor(data_model$Headcahe)
+data_model$loss_smell_taste   <- as.factor(data_model$Loss.of.smell.and.taste)
+data_model$muscle_ache  <- as.factor(data_model$Muscle.Ache)
+data_model$nasal_congestion <- as.factor(data_model$Nasal.Congestion)
+data_model$nausea_vomiting  <- as.factor(data_model$Nausea.and.Vomiting)
+data_model$self_diagnosis <- as.factor(data_model$Self.Diagnosis)
+data_model$shortness_breath <- as.factor(data_model$Shortness.of.Breath)
+data_model$sore_throat <- as.factor(data_model$Sore.Throat)
+data_model$sputum <- as.factor(data_model$Sputum)
+data_model$temperature  <- as.factor(data_model$Temperature)
+data_model$health_care_worker <- as.factor(data_model$Healthcare.Worker)
+data_model$care_home_worker <- as.factor(data_model$Care.Home.Worker)
 
-# #### Refactor the levels ##################################################
-# data_selected <- data_model %>%
-#   dplyr::mutate(Covid_tested = dplyr::recode(tested_or_not, !!!level_key),
-#                 chills = forcats::fct_recode(Chills, !!!level_key_chills),
-#                 cough = forcats::fct_recode(Cough, !!!level_key_cough),
-#                 diarrhoea = forcats::fct_recode(Diarrhoea, !!!level_key_diarrhoea),
-#                 fatigue = forcats::fct_recode(Fatigue, !!!level_key_fatigue),
-#                 headache = forcats::fct_recode(Headcahe, !!!level_key_headache),
-#                 loss_smell_taste = forcats::fct_recode(Loss.of.smell.and.taste, !!!level_key_loss_smell_taste),
-#                 muscle_ache = forcats::fct_recode(Muscle.Ache, !!!level_key_muschle_ache),
-#                 nasal_congestion = forcats::fct_recode(Nasal.Congestion, !!!level_key_nasal_congestion),
-#                 nausea_vomiting = forcats::fct_recode(Nausea.and.Vomiting, !!!level_key_nausea_vomiting),
-#                 self_diagnosis = forcats::fct_recode(Self.Diagnosis, !!!level_key_self_diagnosis),
-#                 shortness_breath = forcats::fct_recode(Shortness.of.Breath, !!!level_key_short_breath),
-#                 sore_throat = forcats::fct_recode(Sore.Throat, !!!level_key_sore_throat),
-#                 sputum = forcats::fct_recode(Sputum, !!!level_key_sputum),
-#                 temperature = forcats::fct_recode(Temperature, !!!level_key_temperature),
-#                 care_home_worker = forcats::fct_recode(Care.Home.Worker, !!!level_key_care_home_worker),
-#                 number_days_symptoms = forcats::fct_recode(Number.Of.Days.Symptoms.Showing, !!!level_no_days_sympt_show))
+data_model$asthma   <- as.factor(data_model$`Asthma (managed with an inhaler)`)
+data_model$diabetes_type_two <- as.factor(data_model$`Diabetes Type 2`)
+data_model$obesity <- as.factor(data_model$Obesity)
+data_model$hypertension  <- as.factor(data_model$`High Blood Pressure (hypertension)`)
+data_model$heart_disease  <- as.factor(data_model$`Long-Standing Heart Disease`)
+data_model$kidney_disease <- as.factor(data_model$`Long-Standing Kidney Disease`)
+data_model$lung_condition <- as.factor(data_model$`Long-Standing Lung Condition`)
+data_model$liver_disease <- as.factor(data_model$`Long-Standing Liver Disease`)
+data_model$diabetes_type_one <- as.factor(data_model$`Diabetes Type 1 (controlled by insulin)`)
+data_model$how_unwell <- as.factor(data_model$How.Unwell)
+data_model$age <- as.factor(data_model$Age)
+data_model$covid_tested <- as.factor(data_model$Covid_tested)
+
 
 
 # data_selected$number_days_symptoms <- as.numeric(data_selected$number_days_symptoms)
@@ -320,47 +304,47 @@
 
  
 # #### Refactor the levels ##################################################
-# data_sel<- data_selected %>%
-#   dplyr::rename(location = Location, date_completed = Date.Completed, pregnant = Pregnant, id = ID) %>%
-#   dplyr::select(id,
-#                 covid_tested,
-#                 tested_or_not,
-#                 age,
-#                 gender,
-#                 country,
-#                 location,
-#                 date_completed,
-#                 care_home_worker,
-#                 health_care_worker,
-#                 how_unwell,
-#                 self_diagnosis,
-#                 chills,
-#                 cough,
-#                 diarrhoea,
-#                 fatigue,
-#                 headache,
-#                 how_unwell,
-#                 loss_smell_taste,
-#                 muscle_ache,
-#                 nasal_congestion,
-#                 nausea_vomiting,
-#                 number_days_symptoms,
-#                 pregnant,
-#                 self_diagnosis,
-#                 shortness_breath,
-#                 sore_throat,
-#                 sputum,
-#                 temperature,
-#                 asthma,
-#                 diabetes_type_one,
-#                 diabetes_type_two,
-#                 obesity,
-#                 hypertension,
-#                 heart_disease,
-#                 lung_condition,
-#                 liver_disease,
-#                 hypertension,
-#                 kidney_disease)
+data_sel<- data_model %>%
+  dplyr::rename(location = Location, date_completed = Date.Completed, pregnant = Pregnant, id = ID) %>%
+  dplyr::select(id,
+                covid_tested,
+                tested_or_not,
+                age,
+                gender,
+                country,
+                location,
+                date_completed,
+                care_home_worker,
+                health_care_worker,
+                how_unwell,
+                self_diagnosis,
+                chills,
+                cough,
+                diarrhoea,
+                fatigue,
+                headache,
+                how_unwell,
+                loss_smell_taste,
+                muscle_ache,
+                nasal_congestion,
+                nausea_vomiting,
+                number_days_symptoms,
+                pregnant,
+                self_diagnosis,
+                shortness_breath,
+                sore_throat,
+                sputum,
+                temperature,
+                asthma,
+                diabetes_type_one,
+                diabetes_type_two,
+                obesity,
+                hypertension,
+                heart_disease,
+                lung_condition,
+                liver_disease,
+                hypertension,
+                kidney_disease)
 
-#write.csv(data_sel, file = "/Users/gabrielburcea/Rprojects/data/data_descr_15062020.csv", row.names = FALSE)
+write.csv(data_sel, file = "/Users/gabrielburcea/Rprojects/data/data_descr_15062020.csv", row.names = FALSE)
 

1f50ab12afd6d39125165d6b82fd12ed3ac3de1d
healthpredict <gburcea@ic.ac.uk>
2020-10-15 17:44:25 +0100

    replaced adjusted_preexisting_rates with plot_adjusted_preexisting_rates

---
+++ R/plot_adjusted_preexisting_rates.R
@@ -0,0 +1,57 @@
+#' plot_adjusted_preexisting_rates
+#'
+#' @param data 
+#' @param title 
+#' @param plot_chart 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot_adjusted_preexisting_rates <- function(data, title = "Exampele", plot_chart = TRUE){
+  
+  
+  adjusted_comorbididity_rates_select <- adjusted_comorbidity_rates %>%
+    dplyr::filter(age_recoded_band == "20-39") %>%
+    dplyr::select(country, comorbidities, standardised_existing_cond_rate) 
+    
+  
+  
+  # pre_existing_levels <- c(
+  # 
+  #   "diabetes type two" = "diabetes_type_two", 
+  #   "diabetes type one" = "diabetes_type_one", 
+  #   "heart disease" = "heart_disease", 
+  #   "lung condidition" = "lung_condition", 
+  #   "liver disease" = "liver_disease",
+  #   "kidney disease" = "kidney_disease"
+  #   
+  # )
+  # 
+  # 
+  # adj_comorb_forcats <- adjusted_comorbididity_rates_select %>%
+  #   dplyr::mutate("Pre-existing conditions" = forcats::fct_recode("Pre-existing conditions", !!!pre_existing_levels))
+  
+  cbbPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7", "#CC6600")
+  
+  
+  plot_adjusted_rates <- ggplot2::ggplot(adjusted_comorbididity_rates_select, 
+                                         ggplot2::aes(country, standardised_existing_cond_rate)) +
+    ggplot2::coord_flip() +
+    ggplot2::geom_bar(ggplot2::aes(fill = comorbidities), width = 0.4,
+                            position = position_dodge(width = 0.5), stat = "identity") +
+    ggplot2::scale_fill_manual(values = cbbPalette) + 
+    ggplot2::labs(title = title,
+                  subtitle = "Adjusted age rates for pre-existing conditions in %",
+                  x = "Countries/Adjusted Rates", y = "Percentage", caption = "Source: Your.md data") +
+    ggplot2::theme(axis.title.y = ggplot2::element_text(margin = ggplot2::margin(t = 0, r = 21, b = 0, l = 0)),
+                   plot.title = ggplot2::element_text(size = 12, face = "bold"),
+                   plot.subtitle = ggplot2::element_text(size = 10),
+                   legend.position = "bottom" , legend.box = "horizontal") +
+    ggplot2::theme_bw()
+  
+  plot_adjusted_rates
+  
+}
+
+

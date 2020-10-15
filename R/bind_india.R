020b8d6157c40771e9a61f518e6982c002dc1cb3
healthpredict <gburcea@ic.ac.uk>
2020-10-15 15:34:58 +0100

    added scripts on binding data

---
+++ R/bind_india.R
@@ -0,0 +1,9 @@
+# india_data <- bind_rows(india_asthma, india_diabetes_type_two)
+# india_data <- bind_rows(india_data, brazil_diabetes_type_one)
+# india_data <- bind_rows(india_data, india_hypertension)
+# india_data <- bind_rows(india_data, india_heart_disease)
+# india_data <- bind_rows(india_data, india_kidney_disease)
+# india_data <- bind_rows(india_data, india_liver_disease)
+# india_data <- bind_rows(india_data, india_lung_condition)
+# india_data <- bind_rows(india_data, india_obesity)
+
---
+++ R/bind_mexico.R
@@ -0,0 +1,13 @@
+# mexico_data <- bind_rows(mexico_asthma, mexico_diabetes_type_two)
+# mexico_data <- bind_rows(mexico_data, brazil_diabetes_type_one)
+# mexico_data <- bind_rows(mexico_data, mexico_hypertension)
+# mexico_data <- bind_rows(mexico_data, mexico_heart_disease)
+# mexico_data <- bind_rows(mexico_data, mexico_kidney_disease)
+# mexico_data <- bind_rows(mexico_data, mexico_liver_disease)
+# mexico_data <- bind_rows(mexico_data, mexico_lung_condition)
+# mexico_data <- bind_rows(mexico_data, mexico_obesity)
+# 
+# adjusted_comorbididity_rates <- bind_rows(india_data, brazil_data, mexico_data)
+# 
+# 
+# write.csv(adjusted_comorbididity_rates, file = "/Users/gabrielburcea/rprojects/data/your.md/ajusted_comorbidity_rates.csv", row.names = FALSE)
